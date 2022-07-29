a6c95e7efbe77bd8411ffde8de46d8fe289032f5
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-28 21:18:55 -0300

    random working

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wikiPage, name="wikiPage"),
     path("new-page", views.newPage, name="newPage"),
-    path("edit-page", views.editPage, name="editPage")
+    path("edit-page", views.editPage, name="editPage"),
+    path("random-page", views.randomPage, name="randomPage"),
 ]
