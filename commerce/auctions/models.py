566e7ccf31cd1819f7aa9ec9218d186fdff68e48
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-31 22:07:36 -0300

    first model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class AuctionBids(models.Model):
+    id = models.IntegerField()
+
+
+class AuctionItem(models.Model):
+    name = models.CharField(max_length=64)
+    basePrice = models.FloatField()
+    pictureUrl = models.CharField(max_length=200)
+    description = models.CharField(max_length=254)
+    createdDate = models.DateField()
+    bids = models.ForeignKey(AuctionBids)
+
+
+class Bid(models.Model):
+    value = models.IntegerField()
